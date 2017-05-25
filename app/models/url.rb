9bb70dbb92d76b7cc2e39d76bb41de794c88d13a
tonychinmy <tonychin0101@gmail.com>
2017-05-25 18:32:18 +0800

    Almost Complete

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,7 @@
 class Url < ApplicationRecord
+	validates :long_url, format: { with: /https{0,1}:\/\/.*/,
+    message: "must have http:" }
+	def shorten
+		self.short_url = SecureRandom.hex(5)
+	end
 end
